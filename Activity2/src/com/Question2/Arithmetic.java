7a0b14e2cddca29206aa4c39552ab8a698cf8061
ANUJA <ANUJA@LAPTOP-DF55BG0K>
2022-11-26 13:33:19 +0530

    lab2

---
+++ Activity2/src/com/Question2/Arithmetic.java
@@ -0,0 +1,18 @@
+//Author-Anuja Yedke
+//to display arithmetic addition
+
+package com.Question2;
+
+	//Write your code here
+	class Arithmetic{
+	    public int add(int a, int b){
+	        int sum = a + b; 
+	        return sum;
+	    }
+	}
+
+	class Adder extends Arithmetic{  
+	    public int callAdd(int a, int b){
+	        return add(a, b);
+	    }
+	}
