82378e2d4c22330fc170d5553de19cbdc385bff3
ANUJA <ANUJA@LAPTOP-DF55BG0K>
2022-12-01 19:15:22 +0530

    activity

---
+++ Activity3/src/com/question1/ReverseNumber.java
@@ -0,0 +1,14 @@
+//Author:Anujayedke
+package com.question1;
+
+public class ReverseNumber extends SumofTheDigit {
+
+		public static void main(String[] args) {
+			ReverseNumber dr=new ReverseNumber();
+			dr.reverseNumber();
+			dr.sumIs();
+			
+		}
+
+	}
+	
